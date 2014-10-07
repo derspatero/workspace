68f282b01cc766cf4ec229571e25cc34d1b5fc22
Edward Lambke <elambke@yahoo.com>
2014-10-06 22:48:13 -0700

    initial commit

---
+++ ca.bcit.comp2052.a00892244.androidhelloworld/gen/com/example/ca/bcit/comp2052/a00892244/androidhelloworld/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.ca.bcit.comp2052.a00892244.androidhelloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
